cce7c87be893ad1c07275b9981e4ea0f0ee9ca04
Colten Appleby <colten96@gmail.com>
2021-02-15 23:45:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,10 @@ def key_for_min_value(name_hash)
   min = 999999999999999999999999999
   min_key = ''
   name_hash.each do |key_name, int|
-    if name_hash[key_name] < min
-      min = name_hash[key_name]
+    if int < min
+      min = int
       min_key = key_name
-
+    end
+  end
+  return min_key
 end
